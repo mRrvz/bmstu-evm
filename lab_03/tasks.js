7d271b9f3457ff8cc1dfade6a127e9c19b15aae5
Romanov Alexey <romanov.alexey2000@gmail.com>
2020-09-20 21:44:12 +0300

    ADD: lab03

---
+++ lab_03/tasks.js
@@ -0,0 +1,116 @@
+"use strict";
+
+const readline = require("readline-sync");
+const path = require("path")
+const fs = require("fs");
+
+const VOWELS = ['A', 'E', 'I', 'O', 'U', 'Y'];
+
+function task1() {
+    const fname = path.join(__dirname, "task1.txt")
+    const n = readline.questionInt("Enter N: ");
+
+    let storage = Array.from(Array(n).keys()).reduce((acc, i) => {
+         const str = readline.question(`Enter string №${i + 1}: `);
+         return str.length % 2 == 0 ? acc.concat([str]) : acc;
+    }, []);
+
+    fs.writeFileSync(fname, JSON.stringify(storage, null, 4));
+}
+
+function task2() {
+    const fname = path.join(__dirname, "task1.txt")
+    const strings = JSON.parse(fs.readFileSync(fname)).filter(str =>
+        !str.toUpperCase().split("").reduce((acc, char) =>
+            VOWELS.includes(char) ? acc + false : acc + true, false)
+    );
+
+    strings.map(str => console.log(str))
+}
+
+function task3() {
+    const extension = readline.question("Enter files extension: ");
+    const dir = readline.question("Enter folder: ");
+
+    fs.readdirSync(folderpath).map(
+        filename => path.join(dir, filename)).map(
+            file => { if (file.endsWith(extension)) {
+                console.log(`File: ${file}\nContents:\n${fs.readFileSync(file)}`);
+        }
+    })
+}
+
+function walk_and_find(fname, correct) {
+    if (fs.lstatSync(fname).isDirectory()) {
+        fs.readdirSync(fname).map(f => walk_and_find(path.join(fname, f), correct));
+    } else if (fs.readFileSync(fname).length <= 10) {
+        correct.push(fname);
+    }
+}
+
+function task4() {
+    const dir = readline.question("Enter path to folder: ");
+    const names = [];
+
+    walk_and_find(dir, names);
+    names.map(name => console.log(name));
+}
+
+function task5() {
+    const n = readline.questionInt("Enter number of strings: ");
+    const fname = path.join(__dirname, "task5.txt")
+    const merged = Array.from(Array(n).keys()).reduce((acc, i) =>
+        acc + fs.readFileSync(readline.question(`Enter string №${i + 1}: `)), "")
+
+    fs.writeFileSync(fname, merged);
+}
+
+function task6() {
+
+}
+
+function max_branch(obj, saver) {
+    if (!["array", "object"].includes(typeof(obj))) {
+        if (saver.current_depth > saver.max_depth) {
+            saver.max_branch = saver.current_branch.filter(t => true);
+            saver.current_depth = saver.max_depth;
+        }
+    } else {
+        ++saver.current_depth;
+
+        Object.keys(obj).map(branch => {
+            saver.current_branch.push(branch);
+            max_branch(obj[branch], saver);
+            saver.current_branch.pop();
+        })
+
+        --saver.current_depth;
+    }
+}
+
+function task7() {
+    const fname = readline.question("Enter filename: ");
+    const obj = JSON.parse(fs.readFileSync(fname));
+    const saver = {
+        current_depth: 0,
+        current_branch: ["/"],
+        max_depth: 0,
+        max_branch: []
+    }
+
+    max_branch(obj, saver);
+
+    console.log("Max branch:", saver.max_branch);
+}
+
+
+function main() {
+    //task1();
+    //task2();
+    //task3();
+    //task4();
+    //task5();
+    task7();
+}
+
+main();
